49704d270f53bc6e9035da9172c1c510ba6dfdcd
pchyuk <pch6590@gmail.com>
2025-05-07 15:08:31 +0900

    remove api key

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body");
 
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg", "8.jpeg", "9.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
